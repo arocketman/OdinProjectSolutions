83a30320a0563e282e87dc980011b087979d8240
Andrea Capuano <andrealfie@gmail.com>
2015-10-26 18:05:47 +0100

    Fixed range usage for elengance purpose

--- substrings.rb
+++ substrings.rb
@@ -1,7 +1,7 @@
 def substrings(word,dictionary)
 	matches = Hash.new(0)
 	word.split.each do |x|
-		for w in (0..dictionary.size-1)
+		for w in (0...dictionary.size)
 				matches[dictionary[w]] += 1 if x == dictionary[w]
 		end
 	end
@@ -9,4 +9,4 @@ def substrings(word,dictionary)
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-puts substrings("going to sit below the desk , below it", dictionary)
+puts substrings("going to sit below the desk , below it below sit", dictionary)
