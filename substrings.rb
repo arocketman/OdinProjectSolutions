b788a4dd3452cdea6d31c4ab1a7cdc04b9a74f42
Andrea Capuano <andrealfie@gmail.com>
2015-10-26 16:23:17 +0100

    Solution for substrings problem

---
+++ substrings.rb
@@ -0,0 +1,12 @@
+def substrings(word,dictionary)
+	matches = Hash.new(0)
+	word.split.each do |x|
+		for w in (0..dictionary.size-1)
+				matches[dictionary[w]] += 1 if x == dictionary[w]
+		end
+	end
+	return matches
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+puts substrings("going to sit below the desk , below it", dictionary)
